91ce90cb821fa5eac97c420c09f36931381de441
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-09-16 13:50:41 -0500

    Defs & Speech ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-document.getElementsByName("search-button").onclick = function () {
+var numDefs= 0;
+var numUse= 0;
+document.getElementById("search-button").onclick = function () {
 
 	let word = document.getElementById("word-input").value;
 
@@ -7,7 +9,31 @@ document.getElementsByName("search-button").onclick = function () {
 		.then(response => response.json())
 		.then(data => {
 			console.log(data);
-			//document.getElementById("dog-image").setAttribute("src", data.message);
+			data.forEach((word) =>{
+				let wordInfo = word.meanings;
+				console.log(wordInfo);
+				wordInfo.forEach((key)=> {
+					let definitions = key.definitions;
+					let partOfSpeech = key.partOfSpeech;
+
+					numUse += 1;
+					let div = document.createElement("div");
+					let label = document.createElement("label");
+					label.setAttribute("id", "partOfSpeech-"+numUse)
+					label.innerHTML = numUse+". "+partOfSpeech;
+					div.appendChild(label)
+					document.getElementById("use-col").appendChild(div);
+					console.log(partOfSpeech);
+
+					definitions.forEach((key) => {
+						numDefs += 1;
+						let label = document.createElement("label");
+						label.setAttribute("id", "def-"+numDefs)
+						label.innerHTML = numDefs+". "+key.definition;
+						document.getElementById("defs-col").appendChild(label);
+					});
+				});
+			});
 		});	
 }
 
